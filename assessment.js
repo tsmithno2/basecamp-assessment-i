9fadfa08d21bc2933af2fd5f5876789f44ec9434
Trevor Smith <38140715+tsmithno2@users.noreply.github.com>
2018-04-24 16:50:51 -0600

    Update assessment.js
    my first try!!

--- assessment.js
+++ assessment.js
@@ -1,46 +1,57 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-// CODE HERE
+ var myName = "Trevor Smith"; // CODE HERE
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
-// CODE HERE
+var faveNum = 2;// CODE HERE
 
 // #3 create a variable called 'lovesCode' and set it to true
 
-// CODE HERE
+var lovesCode = true; // CODE HERE
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
-// CODE HERE
+var me = "My name is " + myName + " and my favorite number is " + faveNum +".";// CODE HERE
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
-// CODE HERE
+var trueFaveNum;
+if (faveNum === trueFaveNum){trueFaveNum = true};// CODE HERE
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
-// CODE HERE
+var notHakeem = "Hakeem Olajuwon";
+console.log(notHakeem === myName);// CODE HERE
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
 
 
-// CODE HERE
+if (age >= 16){conDrive = true};
+else {canDrive = false};
+ // CODE HERE
 
 // #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
 
 var colorResponse;
 
 var responseCreator = function(param){
-  // CODE HERE
+ if ("green"){console.log("Green is okay.")};
+ else if ("red"){console.log("I heart red."};
+ else {console.log("What is your faverite color?")};// CODE HERE
 }
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
-// CODE HERE
+function sum (number){
+console.log(number + number)
+};// CODE HERE
 
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
-// CODE HERE
+function greeter (name, number){
+console.log ("Hi! My name is "+name+" and I am "+number+" years old.") 
+};
+greeter (Bryan, 28)// CODE HERE
